dedb1aeadf5e89cf859bc045bd8625fd3eddb3f0
Jay Fairouz <jay07@live.nl>
2022-10-31 16:12:21 +0100

    Fixed an issue where the lightning on the maze was strange and increased the vertices limit of the maze mesh

--- Assets/Scripts/MazeGeneration/GenerateMaze.cs
+++ Assets/Scripts/MazeGeneration/GenerateMaze.cs
@@ -63,6 +63,8 @@ public class GenerateMaze : MonoBehaviour
     private void DrawMesh()
     {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
         List<Vector3> newVertices = new List<Vector3>();
 
         for (int i = 0; i < cells.Count; i++)
@@ -77,7 +79,25 @@ public class GenerateMaze : MonoBehaviour
             newVertices.Add(cells[i].position + new Vector3(1f, 0f, 1f));
             newVertices.Add(cells[i].position + Vector3.forward);
 
-            Debug.Log("i: " + i + " position: " + cells[i].position);
+            newVertices.Add(cells[i].position);
+            newVertices.Add(cells[i].position + Vector3.right);
+            newVertices.Add(cells[i].position + new Vector3(1f, 1f, 0f));
+            newVertices.Add(cells[i].position + Vector3.up);
+
+            newVertices.Add(cells[i].position + new Vector3(0f, 1f, 1f));
+            newVertices.Add(cells[i].position + new Vector3(1f, 1f, 1f));
+            newVertices.Add(cells[i].position + new Vector3(1f, 0f, 1f));
+            newVertices.Add(cells[i].position + Vector3.forward);
+
+            newVertices.Add(cells[i].position);
+            newVertices.Add(cells[i].position + Vector3.right);
+            newVertices.Add(cells[i].position + new Vector3(1f, 1f, 0f));
+            newVertices.Add(cells[i].position + Vector3.up);
+
+            newVertices.Add(cells[i].position + new Vector3(0f, 1f, 1f));
+            newVertices.Add(cells[i].position + new Vector3(1f, 1f, 1f));
+            newVertices.Add(cells[i].position + new Vector3(1f, 0f, 1f));
+            newVertices.Add(cells[i].position + Vector3.forward);
         }
 
         mesh.vertices = newVertices.ToArray();
@@ -87,88 +107,56 @@ public class GenerateMaze : MonoBehaviour
         for (int i = 0; i < cells.Count; i++)
         {
             // Front Quad
-            newTriangles.Add(i * 8); // BottomLeftVertex
-            newTriangles.Add(i * 8 + 2); // TopLeftVertex
-            newTriangles.Add(i * 8 + 1); // BottomRightVertex
-            newTriangles.Add(i * 8); // BottomRightVertex
-            newTriangles.Add(i * 8 + 3); // TopLeftVertex
-            newTriangles.Add(i * 8 + 2); // TopRightVertex
+            newTriangles.Add(i * 24); // BottomLeftVertex
+            newTriangles.Add(i * 24 + 2); // TopLeftVertex
+            newTriangles.Add(i * 24 + 1); // BottomRightVertex
+            newTriangles.Add(i * 24); // BottomRightVertex
+            newTriangles.Add(i * 24 + 3); // TopLeftVertex
+            newTriangles.Add(i * 24 + 2); // TopRightVertex
 
             // Top Quad
-            newTriangles.Add(i * 8 + 2); // BottomLeftVertex
-            newTriangles.Add(i * 8 + 3); // TopLeftVertex
-            newTriangles.Add(i * 8 + 4); // BottomRightVertex
-            newTriangles.Add(i * 8 + 2); // BottomRightVertex
-            newTriangles.Add(i * 8 + 4); // TopLeftVertex
-            newTriangles.Add(i * 8 + 5); // TopRightVertex
+            newTriangles.Add(i * 24 + 10); // BottomLeftVertex
+            newTriangles.Add(i * 24 + 11); // TopLeftVertex
+            newTriangles.Add(i * 24 + 12); // BottomRightVertex
+            newTriangles.Add(i * 24 + 10); // BottomRightVertex
+            newTriangles.Add(i * 24 + 12); // TopLeftVertex
+            newTriangles.Add(i * 24 + 13); // TopRightVertex
 
             // Right Quad
-            newTriangles.Add(i * 8 + 1); // BottomLeftVertex
-            newTriangles.Add(i * 8 + 2); // TopLeftVertex
-            newTriangles.Add(i * 8 + 5); // BottomRightVertex
-            newTriangles.Add(i * 8 + 1); // BottomRightVertex
-            newTriangles.Add(i * 8 + 5); // TopLeftVertex
-            newTriangles.Add(i * 8 + 6); // TopRightVertex
+            newTriangles.Add(i * 24 + 9); // BottomLeftVertex
+            newTriangles.Add(i * 24 + 18); // TopLeftVertex
+            newTriangles.Add(i * 24 + 5); // BottomRightVertex
+            newTriangles.Add(i * 24 + 9); // BottomRightVertex
+            newTriangles.Add(i * 24 + 5); // TopLeftVertex
+            newTriangles.Add(i * 24 + 6); // TopRightVertex
 
             // Left Quad
-            newTriangles.Add(i * 8); // BottomLeftVertex
-            newTriangles.Add(i * 8 + 7); // TopLeftVertex
-            newTriangles.Add(i * 8 + 4); // BottomRightVertex
-            newTriangles.Add(i * 8); // BottomRightVertex
-            newTriangles.Add(i * 8 + 4); // TopLeftVertex
-            newTriangles.Add(i * 8 + 3); // TopRightVertex
+            newTriangles.Add(i * 24 + 8); // BottomLeftVertex
+            newTriangles.Add(i * 24 + 7); // TopLeftVertex
+            newTriangles.Add(i * 24 + 20); // BottomRightVertex
+            newTriangles.Add(i * 24 + 8); // BottomRightVertex
+            newTriangles.Add(i * 24 + 20); // TopLeftVertex
+            newTriangles.Add(i * 24 + 19); // TopRightVertex
 
             // Back Quad
-            newTriangles.Add(i * 8 + 5); // BottomLeftVertex
-            newTriangles.Add(i * 8 + 4); // TopLeftVertex
-            newTriangles.Add(i * 8 + 7); // BottomRightVertex
-            newTriangles.Add(i * 8 + 5); // BottomRightVertex
-            newTriangles.Add(i * 8 + 7); // TopLeftVertex
-            newTriangles.Add(i * 8 + 6); // TopRightVertex
+            newTriangles.Add(i * 24 + 21); // BottomLeftVertex
+            newTriangles.Add(i * 24 + 20); // TopLeftVertex
+            newTriangles.Add(i * 24 + 15); // BottomRightVertex
+            newTriangles.Add(i * 24 + 21); // BottomRightVertex
+            newTriangles.Add(i * 24 + 15); // TopLeftVertex
+            newTriangles.Add(i * 24 + 14); // TopRightVertex
 
             // Bottom Quad
-            newTriangles.Add(i * 8); // BottomLeftVertex
-            newTriangles.Add(i * 8 + 6); // TopLeftVertex
-            newTriangles.Add(i * 8 + 7); // BottomRightVertex
-            newTriangles.Add(i * 8); // BottomRightVertex
-            newTriangles.Add(i * 8 + 1); // TopLeftVertex
-            newTriangles.Add(i * 8 + 6); // TopRightVertex
+            newTriangles.Add(i * 24 + 16); // BottomLeftVertex
+            newTriangles.Add(i * 24 + 22); // TopLeftVertex
+            newTriangles.Add(i * 24 + 23); // BottomRightVertex
+            newTriangles.Add(i * 24 + 16); // BottomRightVertex
+            newTriangles.Add(i * 24 + 17); // TopLeftVertex
+            newTriangles.Add(i * 24 + 22); // TopRightVertex
         }
 
         mesh.triangles = newTriangles.ToArray();
         mesh.Optimize();
         mesh.RecalculateNormals();
-
-        //if (i % 2 == 0) // If i is even
-        //{
-        //// Front triangle
-        //newTriangles.Add(i * 2); // BottomLeftVertex
-        //newTriangles.Add(i * 2 + 2); // TopLeftVertex
-        //newTriangles.Add(i * 2 + 1); // BottomRightVertex
-        //newTriangles.Add(i * 2 + 1); // BottomRightVertex
-        //newTriangles.Add(i * 2 + 2); // TopLeftVertex
-        //newTriangles.Add(i * 2 + 3); // TopRightVertex
-
-        //else
-        //{
-        //    // Front triangle
-        //    newTriangles.Add(i * 2 - 1);
-        //    newTriangles.Add(i * 2 + 1);
-        //    newTriangles.Add(i * 2 + 2);
-        //    newTriangles.Add(i * 2 + 2);
-        //    newTriangles.Add(i * 2 + 1);
-        //    newTriangles.Add(i * 2 + 4);
-        //}
-
-        // Drawing a single quad
-
-        //Mesh mesh = GetComponent<MeshFilter>().mesh;
-        //mesh.vertices = new Vector3[] {
-        //Vector3.zero, Vector3.right, Vector3.up, new Vector3(1f, 1f)
-        //};
-
-        //mesh.triangles = new int[] {
-        //    0, 2, 1, 1, 2, 3
-        //};
     }
 }
